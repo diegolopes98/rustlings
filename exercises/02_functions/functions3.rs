a00edf8e1e2c18a2c6963e73c28bab5b36360ea9
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2026-02-09 18:49:17 -0300

    finish function exercises

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -1,3 +1,5 @@
+use std::u8;
+
 fn call_me(num: u8) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
@@ -6,5 +8,6 @@ fn call_me(num: u8) {
 
 fn main() {
     // TODO: Fix the function call.
-    call_me();
+    call_me(u8::MAX); // should be 255
+    println!("{}", i8::MAX); // should be 127 given one bit is for positive/negative flag
 }
